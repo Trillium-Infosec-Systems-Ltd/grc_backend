796e29c0caf58ff89c269f0cc821a42857453975
maqsoom <maqsoom.ahmed@theowlsense.com>
2025-04-14 19:44:34 +0500

    db_connection

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, APIRouter
+from db import get_neo4j_driver
 
 app = FastAPI()
 router = APIRouter()
@@ -6,5 +7,16 @@ router = APIRouter()
 @router.get("/health")
 def health_check():
     return {"status": "ok"}
+
+
+@router.get("/test-neo4j")
+def test_neo4j():
+    driver = get_neo4j_driver()
+    with driver.session() as session:
+        result = session.run("RETURN 'Neo4j is connected!' AS message")
+        msg = result.single()["message"]
+        return {"neo4j_status": msg}
+
+
 
 app.include_router(router)
